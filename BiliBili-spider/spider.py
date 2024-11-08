2bf2729e96c2f2bbea0fc198cbc310640ec16f51
FIY-pc <2953997758@qq.com>
2024-11-08 22:27:43 +0800

    fix: 删除多余信息

--- BiliBili-spider/spider.py
+++ BiliBili-spider/spider.py
@@ -13,12 +13,6 @@ MAGENTA = '\033[35m'
 CYAN = '\033[36m'
 RESET = '\033[0m'
 
-headers = {
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
-    'referer': 'https://www.bilibili.com',
-    'cookie': "buvid3=A23251E1-2EA9-BDA9-6A6F-292FE1BD971E51823infoc; b_nut=1730965251; _uuid=106883E7B-E8B2-9861-E69E-AFA2871B109DF52790infoc; CURRENT_FNVAL=4048; bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MzEyMjQ0NTMsImlhdCI6MTczMDk2NTE5MywicGx0IjotMX0._WcgxQMe2FCaXrwib5mANbUxnPIfIJO6YutLayT354Q; bili_ticket_expires=1731224393; buvid_fp=96480076448c300b6641ff40e6016fd1; buvid4=2D88C154-AC83-5F33-6452-4EA482710E8353269-024110707-dlMQ19Nmd0LIG7llyBF0dQ%3D%3D; rpdid=|(umJmYJmm)m0J'u~J|mYlklJ; SESSDATA=b91e4b57%2C1746517298%2Ca3f69%2Ab1CjCSn811yRrPU0d4uIgFdsUZ6iMnY_bcHaJgUoebmUz0vNWnSTNgulWnoEujPSXeEPcSVmFGLVNZd0kxSGF3LWdKRzRZZWVzZ0RXcFhRT2RnOG1aMjZMWXNUSXdvOEo2S2REZTk5ak1Lbkh3cUpaeFlENXJSdTFHQ3JQMW0zZDJiMXZYZF8zS3RnIIEC; bili_jct=352459a97484c9416aae523fda001548; DedeUserID=1071575921; DedeUserID__ckMd5=7d2f4ceb0476cb14; bp_t_offset_1071575921=996981558600531968; b_lsid=46F973610_193060CFC67; sid=4iaf6c4r"
-}
-
 class Header:
     def __init__(self,user_agent,referer,cookie):
         self.user_agent = user_agent
